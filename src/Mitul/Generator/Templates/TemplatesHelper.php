fabb12d652ed1d7524436a967127502ea0f592a3
Евгений Глотов <kivagant@gmail.com>
2015-05-19 03:03:14 +0300

    getTemplate() is first trying to use /resources/api-generator-templates/*.txt

--- src/Mitul/Generator/Templates/TemplatesHelper.php
+++ src/Mitul/Generator/Templates/TemplatesHelper.php
@@ -12,7 +12,10 @@ class TemplatesHelper
 {
 	public function getTemplate($template, $type = "Common")
 	{
-		$path = base_path('vendor/mitulgolakiya/laravel-api-generator/src/Mitul/Generator/Templates/' . $type . '/' . $template . '.txt');
+		$path = base_path('resources/api-generator-templates/' . $type . '/' . $template . '.txt');
+		if (!file_exists($path)) {
+			$path = base_path('vendor/mitulgolakiya/laravel-api-generator/src/Mitul/Generator/Templates/' . $type . '/' . $template . '.txt');
+		}
 
 		$fileData = file_get_contents($path);
 
